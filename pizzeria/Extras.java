89aef48335a887e79900fdd2c234d92f995ba569
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2025-02-10 10:27:26 -0600

    Estructurando el proyecto

---
+++ pizzeria/Extras.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Extras {
+
+    public static void limpiarPantalla() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al limpiar la pantalla.");
+        }
+    }
+
+    public static double getCantidad(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        double numero;
+        while (true) {
+            System.out.print(mensaje);
+            String entrada = scanner.nextLine(); // Leer la entrada como String
+            try {
+                numero = Double.parseDouble(entrada); // Intentar convertir la entrada a double
+                break; // Salir del bucle si la conversión es exitosa
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada no válida. Por favor, Intente de Nuevo."); // Mensaje de error
+            }
+        }
+        scanner.close();
+        return numero; // Devolver el número ingresado
+    }
+    
+    public static Integer getNumero(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        Integer numero;
+        while (true) {
+            System.out.print(mensaje);
+            String entrada = scanner.nextLine(); // Leer la entrada como String
+            try {
+                numero = Integer.parseInt(entrada); // Intentar convertir la entrada a double
+                break; // Salir del bucle si la conversión es exitosa
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada no válida. Por favor, Intente de Nuevo."); // Mensaje de error
+            }
+        }
+        scanner.close();
+        return numero; // Devolver el número ingresado
+    }
+}
---
+++ pizzeria/Programa.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class Programa {
+    public static void main(String[] args) {
+        // Crear pizzería
+        Integer opcionMenu;
+        Extras.limpiarPantalla();
+        Pizzeria pizzeria = new Pizzeria();
+        ArrayList<Pizza> pizzas = crearPizzas();
+        ArrayList<Cliente> clientes = crearClientes();
+        pintaBienvenida();
+        pintaMenuPrincipal();
+        opcionMenu = Extras.getNumero("Seleccione una Opción: ");
+
+        // Crear pizzas y añadir al menú
+        // pizzeria.agregarPizzaMenu(hawaiana);
+        // pizzeria.agregarPizzaMenu(pepperoni);
+
+        // Mostrar menú
+        pizzeria.mostrarMenu();
+
+        // Crear pedido
+        // Pedido pedido1 = new Pedido(cliente1);
+        // pedido1.agregarPizza(hawaiana, 2);
+        // pedido1.agregarPizza(pepperoni, 1);
+
+        // Registrar pedido en la pizzería
+        // pizzeria.registrarPedido(pedido1);
+
+        // Mostrar total del pedido
+        // System.out.println("Total del pedido: $" + pedido1.getTotal());
+
+        // Mostrar menú actualizado
+        pizzeria.mostrarMenu();
+    }
+
+    public static ArrayList<Pizza> crearPizzas() {
+        ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
+        Pizza hawaiana = new Pizza("Hawaiana", 8.5, 10);
+        Pizza pepperoni = new Pizza("Pepperoni", 9.5, 5);
+        Pizza tresQuesos = new Pizza("Tres Quesos", 7.5, 3);
+        Pizza mamaMeat = new Pizza("Mama Meat", 10, 5);
+        pizzas.add(hawaiana);
+        pizzas.add(pepperoni);
+        pizzas.add(tresQuesos);
+        pizzas.add(mamaMeat);
+        return pizzas;
+    }
+
+    public static ArrayList<Cliente> crearClientes() {
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        Cliente cliente1 = new Cliente("Juan Perez", "Calle Falsa 128", "9325897542");
+        Cliente cliente2 = new Cliente("Miguel Pérez", "Calle Arroyo 256", "5279432845");
+        Cliente cliente3 = new Cliente("Juan Perez", "Calle Amarilla 512", "3584621897");
+        clientes.add(cliente3);
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+        return clientes;
+    }
+
+    public static void pintaBienvenida() {
+        System.out.println("");
+        System.out.println("Bienvenido a la Pizzeria \"Delit\"");
+        System.out.println("");
+    }
+
+    public static void pintaMenuPrincipal() {
+        System.out.println("");
+        System.out.println("1. Ver menú");
+        System.out.println("2. Ver Clientes");
+        System.out.println("3. Realizar pedido");
+        System.out.println("4. Ver pedidos");
+        System.out.println("5. Salir");
+        System.out.println("");
+    }
+
+}
